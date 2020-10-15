2166cc126644eb84d8090e9f45c1d10c11041dc2
Sierra Wallace <sskwallace@gmail.com>
2020-10-15 01:02:00 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -38,7 +38,13 @@ class Song
     find_by_name(song) || create(song)
   end
   def self.new_from_filename(filename)
-    
+    array = filename.split(" - ")
+    song_name = array[1]
+    artist_name = array[0]
+    genre_name = array[2].split(".mp3").join
+    artist = Artist.find_or_create_by_name(artist_name)
+    genre = Genre.find_or_create_by_name(genre_name)
+    self.new(song_name, artist, genre)
   end
     
     
