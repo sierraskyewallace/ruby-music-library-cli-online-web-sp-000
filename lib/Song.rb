6bcd20b1631463d44c6dc7fd28f8a31a86cbca50
Sierra Wallace <sskwallace@gmail.com>
2020-10-15 01:05:05 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -47,7 +47,7 @@ class Song
     self.new(song_name, artist, genre)
   end
   def self.create_from_filename(filename)
-    self.new_from_filename(filename)
+    self.new_from_filename(filename).save
   end
     
     
