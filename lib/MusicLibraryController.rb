c777c6d6fb04e3776b9e810206d18ed0726d6ffc
Sierra Wallace <sskwallace@gmail.com>
2020-10-15 01:10:39 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -4,12 +4,9 @@ class MusicLibraryController
     @path = path
     MusicImporter.new(path).import
   end
-
   def call
      input = gets.strip
-
      if input != exit
-
      puts "Welcome to your music library!"
      puts "To list all of your songs, enter 'list songs'."
      puts "To list all of the artists in your library, enter 'list artists'."
@@ -19,7 +16,6 @@ class MusicLibraryController
      puts "To play a song, enter 'play song'."
      puts "To quit, type 'exit'."
      puts "What would you like to do?"
-
      case input
          when "list songs"
            list_songs
@@ -36,47 +32,39 @@ class MusicLibraryController
          end
      end
    end
-
   def list_songs
     Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
       puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
     end
   end
-
   def list_artists
     Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |a, i|
       puts "#{i+1}. #{a.name}"
     end
   end
-
   def list_genres
     Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index do |g, i|
       puts "#{i+1}. #{g.name}"
     end
   end
-
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
     input = gets.strip
-
     if artist = Artist.find_by_name(input)
       artist.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
         puts "#{i+1}. #{s.name} - #{s.genre.name}"
       end
     end
   end
-
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
     input = gets.strip
-
     if genre = Genre.find_by_name(input)
       genre.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
         puts "#{i+1}. #{s.artist.name} - #{s.name}"
       end
     end
   end
-
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip.to_i
@@ -86,7 +74,5 @@ class MusicLibraryController
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
-
 end
 
-end
