eaaae4b6b9ced208c7968442309daddc07bb8c62
Sierra Wallace <sskwallace@gmail.com>
2020-10-15 01:21:07 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -6,30 +6,35 @@ class MusicLibraryController
     MusicImporter.new(path).import
   end
   def call
-    input = ""
-    until input == "exit"
-      puts "Please enter action"
-      input = gets.chomp
-      case input
-      when "list songs"
-        list_songs
-      when "list artists" 
-        list_artists
-      when "list genres"
-        list_genres
-      when "play song"
-        play_song
-      when "list artist"
-        list_artist
-      when "list genre"
-        list_genre
-      when "exit"
-        puts "Goodbye" 
-      else
-        puts "invalid action"
-      end
-    end
-  end
+     input = gets.strip
+     if input != exit
+     puts "Welcome to your music library!"
+     puts "To list all of your songs, enter 'list songs'."
+     puts "To list all of the artists in your library, enter 'list artists'."
+     puts "To list all of the genres in your library, enter 'list genres'."
+     puts "To list all of the songs by a particular artist, enter 'list artist'."
+     puts "To list all of the songs of a particular genre, enter 'list genre'."
+     puts "To play a song, enter 'play song'."
+     puts "To quit, type 'exit'."
+     puts "What would you like to do?"
+     case input
+         when "list songs"
+           list_songs
+         when "list artists"
+           list_artists
+         when "list genres"
+           list_genres
+         when "list artist"
+           list_songs_by_artist
+         when "list genre"
+           list_songs_by_genre
+         when "play song"
+           play_song
+         end
+     end
+   end
+artists
+      when "
   def list_songs
     Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
       puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
