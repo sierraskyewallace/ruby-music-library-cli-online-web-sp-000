d93cfd94b807d45c15b8bf728214de6b03f502ff
Sierra Wallace <sskwallace@gmail.com>
2020-10-15 01:21:27 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -33,8 +33,7 @@ class MusicLibraryController
          end
      end
    end
-artists
-      when "
+
   def list_songs
     Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |s, i|
       puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
